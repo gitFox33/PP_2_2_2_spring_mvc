6ce2ade3313b41ed74fb6935581d924a806ed248
Il'nur Rayanov <gitFox33@gmail.com>
2024-01-08 22:24:03 +0500

    удалил импорты

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,6 +1,5 @@
 package web.service;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import web.model.Car;
 
 import java.util.List;
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,11 +1,6 @@
 package web.service;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import web.model.Car;
 
 import java.util.ArrayList;
